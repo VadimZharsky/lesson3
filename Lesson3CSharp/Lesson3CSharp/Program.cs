4f6423561983ee8acee2bcf5aa9b8f5a32ddcf5d
vadim <vadimzharsky1990@gmail.com>
2018-09-25 00:56:45 +0300

    add

--- Lesson3CSharp/Lesson3CSharp/Program.cs
+++ Lesson3CSharp/Lesson3CSharp/Program.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson3CSharp
+namespace lesson3_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hhkhkh");
+            Console.WriteLine("Enter a radius of a circle");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area of this circle is equal to: "+circleArea(rad));
             Console.ReadKey();
+        }
+        static double circleArea(double r)
+        {
+            double pi = Math.PI;
+            double res = pi * (Math.Pow(r, 2));
+            return res;
         }
     }
 }
