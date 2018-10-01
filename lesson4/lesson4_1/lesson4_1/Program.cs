97c8c7605291e674cea71a2ecf5dd5ae90805990
vadim <vadimzharsky1990@gmail.com>
2018-10-01 14:21:11 +0300

    lesson4_1_changed

--- lesson4/lesson4_1/lesson4_1/Program.cs
+++ lesson4/lesson4_1/lesson4_1/Program.cs
@@ -13,32 +13,63 @@ namespace lesson4_1
             string first = Convert.ToString(Console.ReadLine());
             string symbol = Convert.ToString(Console.ReadLine());
             string second = Convert.ToString(Console.ReadLine());
-            
+
             try
             {
                 double x = Convert.ToDouble(first);
                 double y = Convert.ToDouble(second);
                 char symb = Convert.ToChar(symbol);
-                Console.WriteLine("How do you want to make a calculation? :\n1.Through if/else\n2.Through switch/case");
+                Console.WriteLine("How do you want to make a calculation? :\n1.Through if/else\n2.Through switch/case\n3.Through new switch/case");
                 string choice = Convert.ToString(Console.ReadLine());
                 byte selected = Convert.ToByte(choice);
                 if (selected == 1) { Calculated(x, y, symb); }
                 if (selected == 2) { Calc(x, y, symb); }
+                if (selected == 3)
+                {
+                    Expression exp = new Expression(x, y, symb);
+                    NewCalc(exp);
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("incorrect input data");
             }
+
             Console.ReadKey();
+        }
+
+        private static void NewCalc(Expression exp)
+        {
+            string result = "";
+            switch (exp)
+            {
+                case Expression expression when(exp.symb=='+'):
+                    result = $"{exp.x} + {exp.y} = {exp.x + exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '-'):
+                    result = $"{exp.x} - {exp.y} = {exp.x - exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '*'):
+                    result = $"{exp.x} * {exp.y} = {exp.x * exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '/'):
+                    result = $"{exp.x} / {exp.y} = {exp.x / exp.y}";
+                    break;
+                default:
+                    result ="incorrect input data";
+                    break;
+
+            }
+            Console.WriteLine(result);
         }
 
         private static void Calculated(double x, double y, char symbol)
         {
             string result = "";
-            if (symbol == '+') { result = x + " + " + y + " = " + (x + y); }
-            else if (symbol == '-') { result = x + " - " + y + " = " + (x - y); }
-            else if (symbol == '*') { result = x + " * " + y + " = " + (x * y); }
-            else if (symbol == '/') { result = x + " / " + y + " = " + (x / y); }
+            if (symbol == '+') { result = $"{x} + {y} = {x + y}"; }
+            else if (symbol == '-') { result = $"{x} - {y} = {x - y}"; }
+            else if (symbol == '*') { result = $"{x} * {y} = {x * y}"; }
+            else if (symbol == '/') { result = $"{x} / {y} = {x / y}"; }
             else { Console.WriteLine("incorrect input data"); }
             Console.WriteLine(result);
         }
@@ -49,16 +80,16 @@ namespace lesson4_1
             switch (symb)
             {
                 case '+':
-                    result = x + " + " + y + " = " + (x + y);
+                    result = $"{x} + {y} = {x + y}";
                     break;
                 case '-':
-                    result = x + " - " + y + " = " + (x - y);
+                    result = $"{x} - {y} = {x - y}";
                     break;
                 case '*':
-                    result = x + " * " + y + " = " + (x * y);
+                    result = $"{x} * {y} = {x * y}";
                     break;
                 case '/':
-                    result = x + " / " + y + " = " + (x / y);
+                    result = $"{x} / {y} = {x / y}";
                     break;
                 default:
                     result = "incorrect input data";
@@ -66,5 +97,19 @@ namespace lesson4_1
             }
             Console.WriteLine(result);
         }
+    }
+    public class Expression
+    {
+        public double x, y;
+        public char symb;
+
+        public Expression(double x, double y,char symb)
+        {
+            this.x = x;
+            this.y = y;
+            this.symb = symb;
+        }
+
+
     }
 }
