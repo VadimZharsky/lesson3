de55ff2250655fe001584204f9af7eed1041fa58
ronolya <42861357+ronolya@users.noreply.github.com>
2018-10-01 14:31:31 +0300

    Lesson4 (#2)
    * added
    
    * lesson4_completed
    
    * lesson4_1_changed
    
    * changed

---
+++ lesson4/lesson4_0/lesson4_0/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("select  an excercise:\n1.\n2.");            
+            byte selected = Convert.ToByte(Console.ReadLine());
+            if (selected == 1) { ExFirst(); }
+            else if (selected == 2) { ExSecond(); }
+            else { Console.WriteLine("Incorrect input data."); }
+            Console.ReadKey();
+        }
+
+        static void ExFirst()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                if (i % 2 == 0) { Console.WriteLine(i + " is even number");}
+                else { Console.WriteLine(i + " is odd number");  }
+            }
+        }
+
+        static void ExSecond()
+        {
+            int aQuantity = 0;
+            Console.WriteLine("Enter some word");
+            string str = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    aQuantity++;
+                }
+            }
+            Console.WriteLine("Quantity of a letter a in received word is: {0}.", aQuantity);
+        }
+    }
+}
---
+++ lesson4/lesson4_1/lesson4_1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Convert.ToString(Console.ReadLine());
+            string symbol = Convert.ToString(Console.ReadLine());
+            string second = Convert.ToString(Console.ReadLine());
+
+            try
+            {
+                double x = Convert.ToDouble(first);
+                double y = Convert.ToDouble(second);
+                char symb = Convert.ToChar(symbol);
+                Console.WriteLine("How do you want to make a calculation? :\n1.Through if/else\n2.Through switch/case\n3.Through new switch/case");
+                string choice = Convert.ToString(Console.ReadLine());
+                byte selected = Convert.ToByte(choice);
+                if (selected == 1) { Calculated(x, y, symb); }
+                if (selected == 2) { Calc(x, y, symb); }
+                if (selected == 3)
+                {
+                    Expression exp = new Expression(x, y, symb);
+                    NewCalc(exp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" incorrect input data");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void NewCalc(Expression exp)
+        {
+            string result = "";
+            switch (exp)
+            {
+                case Expression expression when(exp.symb=='+'):
+                    result = $"{exp.x} + {exp.y} = {exp.x + exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '-'):
+                    result = $"{exp.x} - {exp.y} = {exp.x - exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '*'):
+                    result = $"{exp.x} * {exp.y} = {exp.x * exp.y}";
+                    break;
+                case Expression expression when (exp.symb == '/'):
+                    result = $"{exp.x} / {exp.y} = {exp.x / exp.y}";
+                    break;
+                default:
+                    result ="incorrect input data";
+                    break;
+
+            }
+            Console.WriteLine(result);
+        }
+
+        private static void Calculated(double x, double y, char symbol)
+        {
+            string result = "";
+            if (symbol == '+') { result = $"{x} + {y} = {x + y}"; }
+            else if (symbol == '-') { result = $"{x} - {y} = {x - y}"; }
+            else if (symbol == '*') { result = $"{x} * {y} = {x * y}"; }
+            else if (symbol == '/') { result = $"{x} / {y} = {x / y}"; }
+            else { Console.WriteLine("incorrect input data"); }
+            Console.WriteLine(result);
+        }
+
+        static void Calc(double x, double y, char symb)
+        {
+            string result = "";
+            switch (symb)
+            {
+                case '+':
+                    result = $"{x} + {y} = {x + y}";
+                    break;
+                case '-':
+                    result = $"{x} - {y} = {x - y}";
+                    break;
+                case '*':
+                    result = $"{x} * {y} = {x * y}";
+                    break;
+                case '/':
+                    result = $"{x} / {y} = {x / y}";
+                    break;
+                default:
+                    result = "incorrect input data";
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+    public class Expression
+    {
+        public double x, y;
+        public char symb;
+
+        public Expression(double x, double y,char symb)
+        {
+            this.x = x;
+            this.y = y;
+            this.symb = symb;
+        }
+
+
+    }
+}
