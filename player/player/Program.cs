6603acdd946bab7a6468bc3f5a04f1f5bc1093da
vadim <vadimzharsky1990@gmail.com>
2018-10-02 06:14:07 +0300

    lesson5

---
+++ player/player/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song [] songs = new Song[34];
+            songs[0] = new Song();
+            songs[1] = new Song();
+             Console.WriteLine(songs[0].locked);
+            songs[0].Lock();
+            Console.WriteLine(songs[0].locked);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ player/player/Song.cs
@@ -0,0 +1,19 @@
+﻿namespace player
+{
+    public class Song
+    {
+        public bool locked, isPlaying = false;
+        public string lyrics
+        {
+            get => "some lyrics";
+        }
+        public bool Lock()
+        {
+            return locked = true;
+        }
+        public bool Unlockock()
+        {
+            return locked = false;
+        }
+    }
+}
