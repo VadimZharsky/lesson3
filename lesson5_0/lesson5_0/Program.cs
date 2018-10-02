6b8f9887eb110599c8ba822d19c2558ccca4a520
vadim <vadimzharsky1990@gmail.com>
2018-10-02 03:00:31 +0300

    Akinator completed

--- lesson5_0/lesson5_0/Program.cs
+++ lesson5_0/lesson5_0/Program.cs
@@ -10,42 +10,50 @@ namespace lesson5_0
     {
         static void Main(string[] args)
         {
-            int num = 6;
-            int count = 1;
-            int result = num;
-            while (count < 8)
-            {
-                result = result * num;
-                count++;
-                Console.WriteLine(result);
-            }
+
             akinator();
             Console.ReadKey();
         }
         static void akinator()
         {
             Console.WriteLine("Akinator from 0 to 100");
-            bool ifDone = false;
-            int part = 50;
-            int temp = 100;
-            int del = 2;
-            while (ifDone == false)
-            {
-                
-                Console.WriteLine($"your number is greater than {temp - temp/del}?\n1.yes\n2.no");
-                byte choise = Convert.ToByte(Console.ReadLine());
-                if (choise == 1)
-                {
-                    part = temp / (del / 2);
-                    Console.WriteLine($"your number is between {part} and {temp - temp / del}");
-                    del *= 2;
-                }
-                else
-                {
-                    Console.WriteLine($"your number is greater than {part}?\n1.yes\n2.no");
-                    // part = 
-                }
-            }
+            int maxEdge = 100;
+            int minEdge = 0;
+            int average = maxEdge / 2;
+            Console.WriteLine($"your number is greater than {average}?\n1.yes\n2.no\n3.No, my answer is equal to {average}");
+            byte choise = Convert.ToByte(Console.ReadLine());
+            if (choise == 1){average = StepUp(maxEdge, minEdge,average);}
+            if (choise == 2){average = StepDown(maxEdge, minEdge, average);}
+            Console.WriteLine("Your number is {0}", average);
+
+        }
+        static int StepUp(int maxEdge, int minEdge, int average)
+        {
+            minEdge = average;
+            average = (maxEdge + minEdge)/2;
+            Console.WriteLine("Should be your number is between {0} and {1}", maxEdge, minEdge);
+            Console.WriteLine($"your number is greater than {average}?\n1.yes\n2.no\n3.No, my answer is equal to {average}");
+            byte choise = Convert.ToByte(Console.ReadLine());
+            if (choise == 1 && !(maxEdge - minEdge <= 2)) { return StepUp(maxEdge, minEdge, average); }
+            if (choise == 1 && (maxEdge - minEdge <= 2)) { return average + 1; }
+            if (choise == 2 && !(maxEdge - minEdge <= 2)) { return StepDown(maxEdge, minEdge, average); }
+            if (choise == 2 && (maxEdge - minEdge <= 2)) { return average; }
+            else { return average; }
+        }
+
+        static int StepDown(int maxEdge, int minEdge, int average)
+        {
+            maxEdge = average;
+            minEdge = (average == 50) ? 0 : minEdge;
+            average = (maxEdge + minEdge) / 2;
+            Console.WriteLine("Should be your number is between {0} and {1}", maxEdge, minEdge);
+            Console.WriteLine($"your number is greater than {average}?\n1.yes\n2.no\n3.No, my answer is equal to {average}");
+            byte choise = Convert.ToByte(Console.ReadLine());
+            if (choise == 1 && !(maxEdge - minEdge <= 2)) { return StepUp(maxEdge, minEdge, average); }
+            if (choise == 1 && (maxEdge - minEdge <= 2)) { return average + 1; }
+            if (choise == 2 && !(maxEdge-minEdge<=2)) { return StepDown(maxEdge, minEdge, average); }
+            if (choise == 2 && (maxEdge - minEdge <= 2)) { return average; }
+            else { return average; }
         }
     }
 }
