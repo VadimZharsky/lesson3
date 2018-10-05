05ab14011d3a96b8809bb36679e0ebf53d2fb7f6
vadim <vadimzharsky1990@gmail.com>
2018-10-05 08:56:17 +0300

    Sort Completed

--- player/player/Playlist.cs
+++ player/player/Playlist.cs
@@ -37,7 +37,6 @@ namespace player
         }
         public void Sorting(List<string> forSort)
         {
-            int counter = 0;
             sorted = forSort;
             for (int i = sorted.Capacity-1; i >= 0; i--)
             {
@@ -45,26 +44,22 @@ namespace player
                 {
                     string temp = sorted[j].ToLower();
                     string temp2 = sorted[j + 1].ToLower();
-                    string forInsertFirst = EachCharSort(temp, temp2, counter);
+                    string forInsertFirst = EachCharSort(temp, temp2);
                     string forInsertSecond = (forInsertFirst == temp) ? temp2 : temp;
                     
                     sorted.Remove(sorted[j]);
                     sorted.Remove(sorted[j]);
                     sorted.Insert(j, forInsertSecond);
                     sorted.Insert(j, forInsertFirst);
-
-
-
-
                 }  
             }
             foreach(string s in sorted)
                 Console.WriteLine("::::::::::::"+s+":::::::::::::::");
 
         }
-        private string EachCharSort(string one, string two, int counter)
+        private string EachCharSort(string one, string two)
         {
-            string fromOne, fromTwo;
+            int counter = 0;
             byte index, index2;
             int lengthOne = one.Length;
             int lengthTwo = two.Length;
@@ -72,7 +67,26 @@ namespace player
             index = (byte)one[counter];
             index2 = (byte)two[counter];
             if (index < index2) { return one; }
-            else{ return two; }
+            else if (index == index2)
+            {
+                return deepSort(one, two, counter, maximum);
+            }
+            else { return two; } 
+        }
+
+        private string deepSort(string one, string two, int counter, int maximum)
+        {
+            counter++;
+            byte index, index2;
+            index = (byte)one[counter];
+            index2 = (byte)two[counter];
+            bool match = (index == index2) ? true : false;
+            if (match == true && counter < maximum-1) { return deepSort(one, two, counter, maximum); }
+            else
+            {
+                if (index < index2) { return one; }
+                else { return two; }
+            }
         }
     }
 }
