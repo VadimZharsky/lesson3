7084171544138e43d8f4ce366fe63a9c0aa6cd72
vadim <vadimzharsky1990@gmail.com>
2018-10-10 05:52:15 +0300

    lesson6Completed

---
+++ lesson6/lesson6(practice)/lesson6(practice)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6_practice_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. считает, сколько символов у стринг. если 13 и более то обрезает
+            //2. работа со стихотворением
+            Texter("Select a part from practice:\n1.Task with String\n2.Task with a poem");
+            byte choise = Convert.ToByte(Console.ReadLine());
+            if (choise == 1) { StringWorker(); }
+            if (choise == 2) { PoemWorker(); }
+            Console.ReadKey();
+        }
+
+        private static void StringWorker()
+        {
+            Texter("\nEnter some phrase below: ...");
+            string phrase = Console.ReadLine();
+            if (phrase.Length > 13)
+            {
+                phrase = $"{phrase.Remove(13)}...";
+                Texter(phrase);
+            }
+            else {Texter(phrase); }
+        }
+
+        private static void PoemWorker()
+        {
+            Texter("\nsome poem...");
+            string conveyor = "цй укеооонллгшщ зхллъфыв атттпр олд жэоячс мить бю.";
+            char[] splitter = conveyor.ToCharArray();
+            string poem ="";
+            Random rnd = new Random();
+            for (int i = 0; i < (rnd.Next(4, 8)); i++)
+            {
+                for(int j = 0; j<=(rnd.Next(20,50));j++)
+                    poem += splitter[rnd.Next(0,conveyor.Length-1)];
+                poem += ',';
+            }
+            Texter(poem);
+            Texter("");
+            string[] separated = poem.Split(',');
+            for (int i = 0; i<separated.Length-1;i++)
+            {
+                separated[i] = insertor(separated[i], 0);
+                separated[i] = separated[i].Replace("л","ЛЬ");
+                separated[i] = separated[i].Replace("о", "А");
+                separated[i] = separated[i].Replace("ть", "Т");
+                Texter(separated[i].TrimStart());
+            }
+               
+        }
+        private static string insertor(string s, int index)
+        {
+            if (s.IndexOf('т',index)>=0)
+            {
+                int ind = s.IndexOf('т', index+1);
+                if (ind < 0) {{ return s; } }
+                s = s.Insert(ind + 1, "ь");
+                return insertor(s, ind);
+            }
+            else { return s; }
+        }
+        public static void Texter(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ lesson6/lesson6(pyatnashki)/lesson6(pyatnashki)/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6_pyatnashki_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sets = new char[] { 'w','a','d','s','q'};
+            int[,] arr = new int[3,3];
+            int size = arr.GetLength(0);
+            int size2 = size;
+            ArrCreator(arr, size, size2);
+            Writer(arr, size, size2);
+            int index1 = 1;
+            int index2 = 1;
+            ArrayGamer(arr, size, size2, sets, index1, index2);
+            Console.ReadKey();
+        }
+
+        private static void ArrayGamer(int[,] arr, int size, int size2, char[] sets, int index1, int index2)
+        {
+            Console.WriteLine("press key to play: ...");
+            string temp = Convert.ToString(Console.ReadLine());
+            try
+            {
+                char choise = Convert.ToChar(temp);
+                switch (choise)
+                {
+                    case 'w':
+                        {
+                            int iter = index1-1;
+                            if ((iter >= 0 && iter <= size - 1) && (index2 >= 0 && index2 <= size2 - 1))
+                            {
+                                arr[index1, index2] = 1;
+                                index1--;
+                                arr[index1,index2]=0;
+                            }
+                            break;
+                        }
+                    case 's':
+                        {
+                            int iter = index1+1;
+                            if ((iter>=0 && iter <= size - 1) && (index2>=0 && index2 <= size2 - 1))
+                            {
+                                arr[index1, index2] = 1;
+                                index1++;
+                                arr[index1, index2] = 0;
+                            }
+                            break;
+                        }
+                    case 'a':
+                        {
+                            int iter = index2-1;
+                            if ((index1 >= 0 && index1 <= size - 1) && (iter >= 0 && iter <= size2 - 1))
+                            {
+                                arr[index1, index2] = 1;
+                                index2--;
+                                arr[index1, index2] = 0;
+                            }
+                            break;
+                        }
+                    case 'd':
+                        {
+                            int iter = index2+1;
+                            if ((index1>=0 && index1 <= size - 1) && (iter>=0 && iter <= size2 - 1))
+                            {
+                                arr[index1, index2] = 1;
+                                index2++;
+                                arr[index1, index2] = 0;
+                            }
+                            break;
+                        }
+                    case 'q':
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Try more");
+                            break;
+                        }
+                }
+                Writer(arr, size, size2);
+                ArrayGamer(arr, size, size2, sets, index1, index2);
+            }
+            catch (Exception) { ArrayGamer(arr, size, size2, sets, index1, index2); }
+            
+        }
+
+        private static void Writer(int[,] arr, int size, int size2)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size2; j++)
+                {
+                    if (j == size2 - 1) { Console.WriteLine(arr[i, j] + " "); }
+                    else { Console.Write(arr[i, j] + " "); }
+                }
+            }
+        }
+
+        private static void ArrCreator(int[,] arr, int size, int size2)
+        {
+            
+            for (int i = 0; i<size; i++)
+            {
+                for (int j = 0; j < size2; j++)
+                {
+                    arr[i, j] = 1;
+                }
+            }
+            arr[1,1]=0;
+        }
+    }
+}
