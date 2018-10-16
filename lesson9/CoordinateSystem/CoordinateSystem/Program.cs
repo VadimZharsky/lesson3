893889171432b5c97aa61e782fb7ee2bf43595d4
vadim <vadimzharsky1990@gmail.com>
2018-10-16 06:21:05 +0300

    lesson9Added

---
+++ lesson9/CoordinateSystem/CoordinateSystem/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinateSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            threeDimentions[] vector = new threeDimentions[2];
+            vector[0] = new threeDimentions(3,-2,-1);
+            vector[1] = new threeDimentions(1, 2, -5);
+            Console.WriteLine(VectorLength(vector));
+            Console.ReadKey();
+        }
+
+        static double VectorLength(threeDimentions[] vector)
+        {
+            double diffX = vector[1].x - vector[0].x;
+            double diffY = vector[1].y - vector[0].y;
+            double diffZ = vector[1].z - vector[0].z;
+            double result = Math.Sqrt(Pow(diffX)+ Pow(diffY)+ Pow(diffZ));
+            return result;
+        }
+
+        private static double Pow(double pow)
+        {
+            return Math.Pow(pow, 2);
+        }
+    }
+    struct threeDimentions
+    {
+        public double x, y, z;
+        public threeDimentions(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+    }
+}
---
+++ lesson9/DateTimeDeconst/DateTimeDeconst/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDeconst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            today.Deconst(out string currentTime);
+            Console.WriteLine(currentTime);
+            Console.ReadKey();
+        }
+        
+    }
+     public static class Deconstruct
+    {
+        
+         public static void Deconst(this DateTime today, out string curentTime)
+        {
+            curentTime = today.TimeOfDay.ToString();
+        }
+        
+    }
+}
