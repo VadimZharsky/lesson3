7cd3fe6c8baec76ebbb610aa9899719cd5af41bf
Vadim Zharsky <vadimzharsky1990@gmail.com>
2018-10-22 17:21:42 +0300

    Firstly

---
+++ MarketOnline/MarketOnline/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOnline
+{
+    class Customer
+    {
+        public  void ShoppingTable()
+        {
+            var seller = new Seller(); 
+            List<Item> shoppingTable = new List<Item>();
+            shoppingTable = seller.GetItems();
+            foreach (Item item in shoppingTable)
+            {
+                item.SeeItems();
+            }
+        }
+    }
+}
