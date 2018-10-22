7136b210b3b7ea440a8a08c5a0d435aee8d6b19c
vadim <vadimzharsky1990@gmail.com>
2018-10-23 01:33:08 +0300

    completed

---
+++ FigureSquare/FigureSquare/Figure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureSquare
+{
+    public abstract class Figure
+    {
+        public abstract double CalculateArea();
+
+    }
+    public class Triagle : Figure
+    {
+        public double _base { get; set; }
+        public override double CalculateArea()
+        {
+            return _base * (_base / 2);
+        }
+    }
+    public class Circle : Figure
+    {
+        private const double pi = 3.14;
+        public double rad { get; set; }
+        public override double CalculateArea()
+        {
+            return rad * rad * pi;
+        }
+    }
+    public class Square : Figure
+    {
+        public double side { get; set; }
+        public override double CalculateArea()
+        {
+            return side*side;
+        }
+    }
+}
---
+++ FigureSquare/FigureSquare/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix = new double[10,3];
+            for (int i=0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 0) { matrix[i, j] = new Square() { side = i + 1 }.CalculateArea(); }
+                    if (j == 1) { matrix[i, j] = new Triagle() { _base = i + 1 }.CalculateArea(); }
+                    if (j == 2) { matrix[i, j] = new Circle() { rad = i + 1 }.CalculateArea(); }
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i,j]+"||");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
