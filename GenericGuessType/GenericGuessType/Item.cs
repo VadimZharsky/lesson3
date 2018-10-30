0fb1882d4a97a28a628d833d3ecdde89ec47bc40
vadim <vadimzharsky1990@gmail.com>
2018-10-30 05:25:42 +0300

    Advanced3

---
+++ GenericGuessType/GenericGuessType/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericGuessType
+{
+    class IItem
+    {
+    }
+    class Str : IItem
+    {
+        public string str{get; set;}
+    }
+}
