d447925857e8afc8a5e11accc9acd8e98e110855
vadim <vadimzharsky1990@gmail.com>
2018-11-06 06:56:40 +0300

    Collector

---
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstanceCounter();
+            Console.ReadKey();
+        }
+
+        private static void InstanceCounter()
+        {
+            GC.Collect(0);
+            Console.WriteLine((double)GC.GetTotalMemory(true)/1024);
+        }
+    }
+}
