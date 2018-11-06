4ae702df4a8716d92b6cdfe6dc8d2b3571f10441
vadim <vadimzharsky1990@gmail.com>
2018-11-06 03:09:46 +0300

    File'n'console Srlz completed

---
+++ AL6/AL6/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AL6
+{
+    [Serializable()]
+    public class Song
+    {
+        [XmlElement("title")]
+        public string title { get; set; }
+        [XmlElement("author")]
+        public string author { get; set; }
+        [XmlElement("duration")]
+        public double duration { get; set; }
+        [XmlElement("year")]
+        public string year { get; set; }
+    }
+}
