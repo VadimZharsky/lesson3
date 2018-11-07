debb7c1d579ea06672e71897b9f12ada407deb58
vadim <vadimzharsky1990@gmail.com>
2018-11-07 06:01:01 +0300

    IDisposable almost completed

--- GarbageCollector/GarbageCollector/Program.cs
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -6,18 +6,41 @@ using System.Threading.Tasks;
 
 namespace GarbageCollector
 {
+    using System;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string []args)
         {
-            InstanceCounter();
+            ObjectCreator();
+            Console.WriteLine((double)GC.GetTotalMemory(true)/1024);
+            
+            
             Console.ReadKey();
         }
-
-        private static void InstanceCounter()
+        static void ObjectCreator()
         {
-            GC.Collect(0);
-            Console.WriteLine((double)GC.GetTotalMemory(true)/1024);
+            List<SimpleClass> list = new List<SimpleClass>();
+            for (int i = 0; i < 500000; i++)
+            {
+                SimpleClass simple = new SimpleClass(ShowMessage);
+                
+                
+                //list.Add(simple);
+                //GC.KeepAlive(list);
+                if (i % 50000 == 0)
+                {
+                    //Console.WriteLine(GC.GetGeneration(simple));
+                    simple.GetNum();
+                    simple.Dispose();
+                    simple.GetNum();
+                }
+            }
+        }
+        public static void ShowMessage(string mes)
+        {
+            Console.WriteLine(mes);
         }
     }
+
 }
--- GarbageCollector/GarbageCollector/SimpleClass.cs
+++ GarbageCollector/GarbageCollector/SimpleClass.cs
@@ -6,12 +6,59 @@ using System.Threading.Tasks;
 
 namespace GarbageCollector
 {
-    class SimpleClass
+    public delegate void MessageHandler(string mes);
+    public class SimpleClass : IDisposable
     {
-        private static int num;
+        MessageHandler _del;
+        private static int _num;
+        private bool _disposed = false;
+
         static SimpleClass()
         {
-            num = 1;
+            _num = 0;
+        }
+
+        public void RegMessage(MessageHandler del)
+        {
+            _del = del;
+        }
+
+        public SimpleClass(MessageHandler message)
+        {
+            _del = message;
+            //_del?.Invoke("Construct");
+            _num++;
+            _disposed = false;
+            //if (_num % 50000 == 0)
+            //{
+            //    _del.Invoke(_num.ToString());
+            //    _del.Invoke(Convert.ToString((double)GC.GetTotalMemory(false) / 1024));
+            //}
+        }
+        //~SimpleClass()
+        //{
+        //    _num--;
+        //    //_del?.Invoke("Destruct");
+        //    //_del?.Invoke(_num.ToString());
+        //}
+        public void GetNum()
+        {
+            
+            _del?.Invoke($"now \"_disposed\" is {_disposed}");
+        }
+
+        public void Dispose()
+        {
+            ToDispose();
+            _del($"object № {_num} before GC {(double)GC.GetTotalMemory(false) / 1024} Kbytes");
+            GC.Collect();
+            _del($"object № {_num} after GC {(double)GC.GetTotalMemory(false) / 1024} Kbytes");
+            _del?.Invoke("Disposing...");
+        }
+
+        private void ToDispose()
+        {
+            _disposed = true;
         }
     }
 }
