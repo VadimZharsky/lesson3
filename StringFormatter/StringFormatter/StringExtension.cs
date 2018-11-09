e89415f7f01e63ba2a0890d396f11c0fdd9ba99a
vadim <vadimzharsky1990@gmail.com>
2018-11-09 03:07:38 +0300

    Delegates are completed

---
+++ StringFormatter/StringFormatter/StringExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static StringFormatter.Program;
+
+namespace StringFormatter
+{
+    static class StringExtension
+    {
+        public static List<string> StringWorker(this stringFormatter formatter, List<string> list)
+        {
+            List<string> vs = new List<string>(5);
+            for (int i = 0; i < list.Count; i++)
+            {
+                formatter = ToUpperCase;
+                string s = formatter(list[i]);
+                formatter = ReplaceSpaceToUnderscore;
+                s = formatter(s);
+                vs.Add(s);
+            }
+            return vs;
+        }
+
+        public static string ToUpperCase(string str)
+        {
+            string temp = str.ToUpper();
+            return temp;
+        }
+        public static string ReplaceSpaceToUnderscore(string str)
+        {
+            if (str.Contains(' '))
+            {
+                string temp = str.Replace(' ', '_');
+                return temp;
+            }
+            return str;
+        }
+
+    }
+}
