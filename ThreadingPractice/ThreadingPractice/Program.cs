84f6497cdaee7f891e614d1328fd72366afd00f7
atormyshev3 <atormyshev3@gmail.com>
2018-11-09 21:14:36 +0300

    fromClassroom

---
+++ ThreadingPractice/ThreadingPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                ThreadPool.QueueUserWorkItem((object state) =>
+                {
+                    lock (rnd)
+                    {
+                        File.AppendAllText($@"temp{i}.txt", $"this is {i}th string\n");
+                        Thread.Sleep(rnd.Next(0, 200));
+                        Console.WriteLine(i);
+                    }
+                    
+                });
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
