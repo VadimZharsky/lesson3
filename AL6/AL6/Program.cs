58294440d05299cb6d2ec91adb50fb35f1003b03
VadimZharsky <42954157+VadimZharsky@users.noreply.github.com>
2018-11-10 00:07:51 +0300

    Al6 (#20)
    * operationsWithFiles
    
    * File'n'console Srlz completed

--- AL6/AL6/Program.cs
+++ AL6/AL6/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
+
 
 namespace AL6
 {
     class Program
     {
+        public delegate void serializ<T>(T item);
+
+
         static void Main(string[] args)
         {
             //DirectoryInfo dir = new DirectoryInfo(@"C:\Program Files\Autodesk");
@@ -18,14 +20,52 @@ namespace AL6
             //FileInfo firstFile = GetFileInfo(dir);
             //Directory.CreateDirectory(addedDir);
             //CopyFirstFile(addedDir,firstFile);
-            Chatt.Chatting();
+
+            //Chatt.Chatting();
+            serializ<Song> del = ConsoleSrlz;
+            del += FileSrlz;
+
+
+            Song song = new Song() { title = "primiere chanson", author = "some author", duration = 334, year = "2018" };
+            del(song);
+
             Console.ReadKey();
         }
 
-        private static void Chatting()
+        private static void ConsoleSrlz(Song song)
         {
-            throw new NotImplementedException();
+            
+            XmlSerializer xs = new XmlSerializer(typeof(Song));
+            MemoryStream memory = new MemoryStream();
+            string str = "";
+            xs.Serialize(Console.Out, song);
+            xs.Serialize(memory, song);
+            byte[] bytes = memory.GetBuffer();
+            foreach (byte b in bytes)
+                str+=(char)b;
+            StringReader reader = new StringReader(str);
+            Song anotherSong = null;
+            anotherSong = (Song)xs.Deserialize(reader);
+            Console.WriteLine($"another song is:::{anotherSong.title}:::{anotherSong.author}:::{anotherSong.year}:::{anotherSong.year}:::{anotherSong.duration}");
         }
+
+
+        private static void FileSrlz(Song song)
+        {
+            FileStream fs = new FileStream("srlz.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Song));
+            xs.Serialize(fs, song);
+            fs.Close();
+            StreamReader reader = new StreamReader("srlz.txt");
+            string s = reader.ReadToEnd();
+            StringReader stringReader = new StringReader(s);
+            Song thatSong =(Song) xs.Deserialize(stringReader);
+            Console.WriteLine(s);
+        
+            Console.WriteLine($"that song is:::{thatSong.title}:::{thatSong.author}:::{thatSong.year}:::{thatSong.year}:::{thatSong.duration}");
+        }
+
+
 
         private static void GetInfoAboutDirectory(DirectoryInfo dir)
         {
