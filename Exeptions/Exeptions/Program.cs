dc5ba8a72fc8c1a7002f05f44120486115ea20d6
atormyshev3 <atormyshev3@gmail.com>
2018-11-12 21:03:39 +0300

    Ex added

---
+++ Exeptions/Exeptions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(x);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
