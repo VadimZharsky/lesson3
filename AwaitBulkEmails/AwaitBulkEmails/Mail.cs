b0c9e7672b7db39d458ab97893eb35142c4e5211
vadim <vadimzharsky1990@gmail.com>
2018-11-14 20:48:28 +0200

    AwaitTrying

---
+++ AwaitBulkEmails/AwaitBulkEmails/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwaitBulkEmails
+{
+    class Mail
+    {
+        public string body;
+        public Mail(string body)
+        {
+            this.body = body;
+        }
+    }
+}
